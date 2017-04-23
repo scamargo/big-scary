de643ef574ed5700fcdf38e27058704f6611b396
julianapeace <julianapeace@users.noreply.github.com>
2017-04-23 11:58:17 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ function showDivs(n, slideClass) {
 
 
 $.getJSON(youtubeUrl, function(json) {
-  for(var i=1;i<4;i++) {
+  for(var i=0;i<4;i++) {
     var id = json["items"][i]["id"]["videoId"];
     videoLinks.push(id);
   }
